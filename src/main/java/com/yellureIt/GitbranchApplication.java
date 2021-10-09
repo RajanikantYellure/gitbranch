b73c32f7c2d099dcf416ea2ddfe341b734002428
rajanikantyellure <rajanikantyellure86@gmail.com>
2021-10-09 12:14:10 +0530

    IES02:String added

--- src/main/java/com/yellureIt/GitbranchApplication.java
+++ src/main/java/com/yellureIt/GitbranchApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitbranchApplication {
-
+String s1="Ashwin";
 	public static void main(String[] args) {
 		SpringApplication.run(GitbranchApplication.class, args);
 	}
